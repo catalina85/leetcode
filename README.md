8d4871cdfee14554a4a039de3128cf96b72ce68c
neo <tianm_buaa@163.com>
2019-07-22 09:19:35 +0800

    _002_AddTwoNumbers

--- README.md
+++ README.md
@@ -21,3 +21,4 @@
 |259|[3Sum Smaller](https://leetcode.com/problems/3sum-smaller)  (*)|[C++](./algorithms/cpp/_259_3SumSmaller/Solutions.cpp)|Medium|
 |018|[4Sum](https://leetcode.com/problems/4sum)|[C++](./algorithms/cpp/_018_4Sum/Solutions.cpp)|Medium|
 |454|[4Sum II](https://leetcode.com/problems/4sum-ii)|[C++](./algorithms/cpp/_454_4SumII/Solutions.cpp)|Medium|
+|002|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers)|[C++](./algorithms/cpp/_002_AddTwoNumbers/Solutions.cpp)|Medium|
---
+++ algorithms/cpp/_002_AddTwoNumbers/Solutions.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *prev = dummy;
+
+        int sum = 0;
+        int carry = 0;
+        while (l1 || l2) {
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+
+            sum = (carry + val1 + val2) % 10;
+            carry = (carry + val1 + val2) / 10;
+            prev->next = new ListNode(sum);
+            prev = prev->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        if (carry > 0) prev->next = new ListNode(carry);
+        return dummy->next;
+    }
+
+    void printList(ListNode *node) {
+        while (node != NULL) {
+            cout << node->val << " ";
+            node = node->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+//    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//    Output: 7 -> 0 -> 8
+//    Explanation: 342 + 465 = 807.
+    ListNode *l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(6);
+
+    ListNode *l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    auto *so = new Solution();
+    ListNode *res = so->addTwoNumbers(l1, l2);
+    so->printList(l1);
+    so->printList(l2);
+    so->printList(res);
+    return 0;
+}
